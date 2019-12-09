f227bd214e8f7f5ebfa9ce27e0cce813e2fc204b
HristoMachikov <hristo2635@abv.bg>
2019-12-09 17:00:15 +0200

    modify Login Comp to func Hook Comp

---
+++ cherry-web-app/src/components/Login/LoginHook.jsx
@@ -0,0 +1,150 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import withForm from '../../shared/hocs/withForm';
+import { ToastContainer, toast } from 'react-toastify';
+
+import * as yup from 'yup'
+
+import { minLength } from '../../config/app-config';
+
+import { useFormControl, getValidationsRunnerForSchema } from '../shared/hocs/withForm';
+
+const validations = {
+    email: yup
+        .string()
+        .required("Въведете E-mail!")
+        .email("Въведете валиден E-mail!"),
+
+    password: yup
+        .string()
+        .required("Въведете парола!")
+        .matches(/^[а-яА-Яa-zA-Z0-9]*[а-яА-Яa-zA-Z0-9]$/, "Паролата трябва да е само с букви и цифри!")
+        .min(minLength, `Паролата трябва да е поне ${minLength} символа!`)
+}
+
+const schema = yup.object().shape(validations);
+
+const validationsRunner = getValidationsRunnerForSchema(schema);
+
+const LoginHook = ({ setLogin, history }) => {
+
+    const emailFormControl = useFormControl('', validations.email);
+    const passwordFormControl = useFormControl('', validations.password);
+    const [serverError, setServerError] = React.useState(null);
+
+  const submitHandler = React.useCallback(() => {
+    validationsRunner({
+      email: emailFormControl.value,
+      password: passwordFormControl.value
+    }).then(data => {
+        setLogin(history, data).catch(error => {
+        if (typeof error === 'object') { throw error; }
+        setServerError(error);
+      });
+
+    }).catch(errors => {
+      if (errors.email) { emailFormControl.setErrors(errors.email); }
+      if (errors.password) { passwordFormControl.setErrors(errors.password); }
+    })
+  }, [emailFormControl, passwordFormControl,
+    //  setServerError, 
+     history, setLogin]);
+
+
+    // class Login extends Component {
+
+    //     onChangeHandler = this.props.controlOnChangeHandlerFactory();
+
+    //     emailOnChangeHandler = this.props.controlChangeHandlerFactory("email");
+    //     passwordOnChangeHandler = this.props.controlChangeHandlerFactory("password");
+
+    //     submitHandler = (event) => {
+    //         event.preventDefault();
+
+    //         this.props.runValidations()
+    //             .then(formData => {
+    //                 const errors = this.props.getFormErrorState();
+
+    //                 const firstError = this.getFirstControlError('email')
+    //                     || this.getFirstControlError('password')
+
+    //                 firstError && toast.warn(firstError, {
+    //                     closeButton: false
+    //                 })
+
+    //                 if (!!errors) { return; }
+    //                 const data = this.props.getFormState();
+    //                 this.props.setLogin(this.props.history, data);
+    //             }).catch(err => {
+    //                 console.log(err)
+    //             });
+    //     }
+
+    //     getFirstControlError = name => {
+    //         const errorState = this.props.getFormErrorState();
+    //         return errorState && errorState[name] && errorState[name][0];
+    //     };
+
+    //     render() {
+    //         const currentInputName = this.props.getFormState().currentName;
+    //         const currentInputChecked = this.props.getFormState()[currentInputName];
+
+    //         const classErrorEmail = (currentInputName === 'email' && currentInputChecked && currentInputChecked.length < minLength) ? " error" : "";
+    //         const classErrorPassword = (currentInputName === 'password' && currentInputChecked && currentInputChecked.length < minLength) ? " error" : "";
+
+    //         return (
+    //             <section className="site-section login">
+    //                 <ToastContainer autoClose={3500} />
+
+    //                 <form onSubmit={this.submitHandler} className="main-form">
+    //                     <fieldset className="main-form-fieldsed">
+    //                         <legend className="main-form-legent">Вход</legend>
+    //                         <p className="form-field email first">
+    //                             <label htmlFor="email">E-mail</label>
+    //                             <input
+    //                                 className={`form-input${classErrorEmail}`}
+    //                                 type="text"
+    //                                 name="email"
+    //                                 id="email"
+    //                                 onChange={this.onChangeHandler}
+    //                             />
+    //                             <span></span>
+    //                         </p>
+    //                         <p className="form-field password last">
+    //                             <label htmlFor="password">Парола</label>
+    //                             <input
+    //                                 className={`form-input${classErrorPassword}`}
+    //                                 type="password"
+    //                                 name="password"
+    //                                 id="password"
+    //                                 onChange={this.onChangeHandler}
+    //                             />
+    //                             <span></span>
+    //                         </p>
+    //                         <p className="form-btn">
+    //                             <button className="primary-btn" type="submit">Вход</button>
+    //                         </p>
+    //                         <p className="question">Имаш ли <Link to="/user/register">Регистрация</Link>?</p>
+    //                     </fieldset>
+    //                 </form>
+    //             </section>
+    //         );
+    //     }
+    // }
+
+    // const schema = yup.object().shape({
+    //     password: yup
+    //         .string()
+    //         .required("Въведете парола!")
+    //         .matches(/^[а-яА-Яa-zA-Z0-9]*[а-яА-Яa-zA-Z0-9]$/, "Паролата трябва да е само с букви и цифри!")
+    //         .min(minLength, `Паролата трябва да е поне ${minLength} символа!`),
+    //     email: yup
+    //         .string()
+    //         .required("Въведете E-mail!")
+    //         .email("Въведете валиден E-mail!")
+
+    // });
+
+    // export default withForm(Login, { email: '', password: '' }, schema);
+}
+export default LoginHook;
--- cherry-web-app/src/shared/hocs/withForm.jsx
+++ cherry-web-app/src/shared/hocs/withForm.jsx
@@ -1,4 +1,70 @@
 import React from 'react';
+//workshop 04
+const runControlValidation = (value, validations) => {
+    return validations.validate(value, { abortEarly: false });
+};
+
+export const getValidationsRunnerForSchema = schema => form => {
+    if (!schema) { return Promise.resolve(); }
+    return schema.validate(form, { abortEarly: false })
+        .then(() => form).catch(err => {
+            const errors = err.inner.reduce((acc, { path, message }) => {
+                acc[path] = (acc[path] || []).concat(message);
+                return acc;
+            }, {});
+            return Promise.reject(errors);
+        });
+}
+
+const getControlChangeHandler = (
+    // validations, setErrors, 
+    setValue, setCurrentName) => {
+    let debounce;
+    return e => {
+        const { value, name } = e.target;
+        if (debounce) { clearTimeout(debounce); debounce = null; }
+        debounce = setTimeout(() => {
+            setValue(value);
+            setCurrentName(name);
+            // runControlValidation(value, validations)
+            //     .then(() => {
+            //         setErrors(undefined);
+            //     })
+            //     .catch(err => {
+            //         setErrors(err.errors);
+            //     });
+            debounce = null;
+        }, 200);
+    };
+};
+
+export const useFormControl = (defaultValue
+    , validations
+) => {
+    const [value, setValue] = React.useState(defaultValue);
+    const [errors, setErrors] = React.useState(undefined);
+    const [currentName, setCurrentName] = React.useState('');
+
+    const changeHandler = React.useCallback(
+        getControlChangeHandler(
+            validations,
+            setValue, setErrors, setCurrentName),
+        [
+            validations,
+            setValue, setErrors, setCurrentName]
+    );
+
+    return React.useMemo(() => ({
+        value,
+        setValue,
+        errors,
+        setErrors,
+        changeHandler,
+        currentName,
+        setCurrentName
+    }), [value, setValue, errors, setErrors, changeHandler, currentName, setCurrentName]);
+};
+//
 
 export default function withForm(Cmp, initialState, schema) {
     return class extends React.Component {
@@ -63,9 +129,9 @@ export default function withForm(Cmp, initialState, schema) {
         };
 
         runControlValidation = name => {
+            if (!schema) { return Promise.resolve(); }
             const currentValue = this.state.form[name];
-            // eslint-disable-next-line no-mixed-operators
-            return (schema && schema.fields[name].validate(currentValue, { abortEarly: false })) || Promise.resolve();
+            return schema && schema.fields[name].validate(currentValue, { abortEarly: false });
         };
 
 
@@ -75,12 +141,10 @@ export default function withForm(Cmp, initialState, schema) {
                 this.setState({ errors: undefined });
                 return this.state.form;
             }).catch(err => {
-
                 const errors = err.inner.reduce((acc, { path, message }) => {
                     acc[path] = (acc[path] || []).concat(message);
                     return acc;
                 }, {})
-                console.log(errors)
                 this.setState({
                     errors
                 })
