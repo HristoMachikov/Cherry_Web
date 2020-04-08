20a262b90d52406342e3af11370fd215512bf755
HristoMachikov <hristo2635@abv.bg>
2020-04-09 01:01:53 +0300

    show change password button

--- cherry-web-app/src/components/Navigation/UserNav/UserNav.jsx
+++ cherry-web-app/src/components/Navigation/UserNav/UserNav.jsx
@@ -1,13 +1,20 @@
 import React, { Fragment } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 import AdminNav from './AdminNav/AdminNav';
 
 function UserNav({ username, isAdmin }) {
     return username ? <Fragment>
         <AdminNav isAdmin={isAdmin} />
-        <li>
-            <NavLink to="/user/logout" activeClassName="selected">Изход</NavLink>
+        <li> <Link>Опции</Link>
+            <ul>
+                <li>
+                    <NavLink to="/user/logout" activeClassName="selected">Изход</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/user/change-password" activeClassName="selected">Смени парола</NavLink>
+                </li>
+            </ul>
         </li>
     </Fragment> : <Fragment>
             <li>
