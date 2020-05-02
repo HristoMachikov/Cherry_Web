38afa44e2d34c2ab85dbb94dce729035f5667736
HristoMachikov <hristo2635@abv.bg>
2020-05-03 01:03:53 +0300

    redirect http to https for production

--- cherry-rest-api/index.js
+++ cherry-rest-api/index.js
@@ -19,5 +19,17 @@ dbConnector().then(() => {
 
     http.createServer(app).listen(config.httpPort, console.log(`Listening on port ${config.httpPort}!`));
 
-    // app.listen(config.port, console.log(`Listening on port ${config.port}!`));
+    // const requestListener = function (req, res) {
+    //     res.writeHead(302, {
+    // 	    'Location': 'https://' + req.headers.host + req.url
+    //     });
+    //     res.end();
+    // }
+    
+    // const server = http.createServer(requestListener);
+    // server.listen(8080);
+
+    // For production in react package.json
+    // "start": "PORT=443 HTTPS=true HOST=getcherry.online react-scripts start"
+
 }).catch(console.error);;
